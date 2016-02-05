b81f41aa74652ff8742bcd6356d07009227a9624
russkiy <russkiy78@gmail.com>
2016-02-05 16:50:47 +0300

    init

---
+++ coin.js
@@ -0,0 +1,31 @@
+/**
+ * Created by russkiy on 02.02.16.
+ */
+
+/*
+* 0.01
+* 0.05
+* 0.10
+* 0.50
+* 1
+* 5
+* 10
+* 50
+* 100
+* 500
+* 1000
+* */
+
+
+var coin = {
+    id : '',
+    rand : '',
+    value: 1.0,
+    bhash : ''
+};
+
+var transaction = {
+    shash: '',
+    rhash : ''
+
+};
